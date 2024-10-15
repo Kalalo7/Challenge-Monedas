01dc6e9da359e801f13cb45341c8ec6d1bf57389
Martin Fauci <martinfauci@gmail.com>
2024-10-15 15:57:50 +0200

    Primer commit y push del proyecto

---
+++ src/ConsultaMoneda.java
@@ -0,0 +1,25 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaMoneda {
+    public Monedas buscarMoneda (String monedaBase, String monedaTarget){
+        URI direccion = URI.create ("https://v6.exchangerate-api.com/v6/fad54cfcea36689eceed629a/pair/"+monedaBase+"/"+monedaTarget);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request= HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Monedas.class);
+        } catch (Exception e) {
+            throw new RuntimeException( "No se ha encontrado la moneda solicitada");
+        }
+    }
+}
